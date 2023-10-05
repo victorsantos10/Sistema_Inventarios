397271f02a5e95c1f60595f1f35acbb44f431e7f
SoftwareMaker1909 <105822309+SoftwareMaker1909@users.noreply.github.com>
2023-10-04 23:21:20 -1100

    Funcion Login

--- SistemaInventarios/Login.Designer.cs
+++ SistemaInventarios/Login.Designer.cs
@@ -28,8 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             System.Windows.Forms.Button LoginBtn;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.UserTextBox = new System.Windows.Forms.TextBox();
             this.PasswordTextBox = new System.Windows.Forms.TextBox();
@@ -42,12 +42,26 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
+            // LoginBtn
+            // 
+            LoginBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            LoginBtn.Font = new System.Drawing.Font("Berlin Sans FB", 20.25F);
+            LoginBtn.Location = new System.Drawing.Point(973, 601);
+            LoginBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            LoginBtn.Name = "LoginBtn";
+            LoginBtn.Size = new System.Drawing.Size(181, 98);
+            LoginBtn.TabIndex = 6;
+            LoginBtn.Text = "Entrar";
+            LoginBtn.UseVisualStyleBackColor = false;
+            LoginBtn.Click += new System.EventHandler(this.LoginBtn_Click);
+            // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(0, -1);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(346, 682);
+            this.pictureBox1.Size = new System.Drawing.Size(461, 839);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -55,17 +69,19 @@
             // UserTextBox
             // 
             this.UserTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
-            this.UserTextBox.Location = new System.Drawing.Point(647, 270);
+            this.UserTextBox.Location = new System.Drawing.Point(863, 332);
+            this.UserTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.UserTextBox.Name = "UserTextBox";
-            this.UserTextBox.Size = new System.Drawing.Size(306, 20);
+            this.UserTextBox.Size = new System.Drawing.Size(407, 22);
             this.UserTextBox.TabIndex = 1;
             // 
             // PasswordTextBox
             // 
             this.PasswordTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
-            this.PasswordTextBox.Location = new System.Drawing.Point(647, 386);
+            this.PasswordTextBox.Location = new System.Drawing.Point(863, 475);
+            this.PasswordTextBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.PasswordTextBox.Name = "PasswordTextBox";
-            this.PasswordTextBox.Size = new System.Drawing.Size(306, 20);
+            this.PasswordTextBox.Size = new System.Drawing.Size(407, 22);
             this.PasswordTextBox.TabIndex = 2;
             this.PasswordTextBox.UseSystemPasswordChar = true;
             // 
@@ -74,9 +90,10 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Berlin Sans FB", 20.25F);
-            this.label1.Location = new System.Drawing.Point(645, 237);
+            this.label1.Location = new System.Drawing.Point(860, 292);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(97, 30);
+            this.label1.Size = new System.Drawing.Size(123, 37);
             this.label1.TabIndex = 3;
             this.label1.Text = "Usuario";
             // 
@@ -85,9 +102,10 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Berlin Sans FB", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(645, 353);
+            this.label2.Location = new System.Drawing.Point(860, 434);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(75, 30);
+            this.label2.Size = new System.Drawing.Size(98, 37);
             this.label2.TabIndex = 4;
             this.label2.Text = "Clave";
             // 
@@ -96,40 +114,31 @@
             this.OlvidoPassword.AutoSize = true;
             this.OlvidoPassword.Font = new System.Drawing.Font("Berlin Sans FB", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.OlvidoPassword.LinkColor = System.Drawing.Color.Black;
-            this.OlvidoPassword.Location = new System.Drawing.Point(765, 657);
+            this.OlvidoPassword.Location = new System.Drawing.Point(1020, 809);
+            this.OlvidoPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.OlvidoPassword.Name = "OlvidoPassword";
-            this.OlvidoPassword.Size = new System.Drawing.Size(83, 15);
+            this.OlvidoPassword.Size = new System.Drawing.Size(116, 19);
             this.OlvidoPassword.TabIndex = 5;
             this.OlvidoPassword.TabStop = true;
             this.OlvidoPassword.Text = "Olvide mi clave";
             // 
-            // LoginBtn
-            // 
-            LoginBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
-            LoginBtn.Font = new System.Drawing.Font("Berlin Sans FB", 20.25F);
-            LoginBtn.Location = new System.Drawing.Point(730, 488);
-            LoginBtn.Name = "LoginBtn";
-            LoginBtn.Size = new System.Drawing.Size(136, 80);
-            LoginBtn.TabIndex = 6;
-            LoginBtn.Text = "Entrar";
-            LoginBtn.UseVisualStyleBackColor = false;
-            // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(683, 8);
+            this.pictureBox2.Location = new System.Drawing.Point(911, 10);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(230, 226);
+            this.pictureBox2.Size = new System.Drawing.Size(307, 278);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 7;
             this.pictureBox2.TabStop = false;
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.ClientSize = new System.Drawing.Size(1683, 828);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(LoginBtn);
             this.Controls.Add(this.OlvidoPassword);
@@ -139,10 +148,10 @@
             this.Controls.Add(this.UserTextBox);
             this.Controls.Add(this.pictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(1280, 720);
-            this.MinimumSize = new System.Drawing.Size(1280, 720);
+            this.MaximumSize = new System.Drawing.Size(1701, 875);
+            this.MinimumSize = new System.Drawing.Size(1701, 875);
             this.Name = "Login";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
--- SistemaInventarios/Login.cs
+++ SistemaInventarios/Login.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,41 @@ namespace SistemaInventarios
 {
     public partial class Login : Form
     {
+        private string connectionString = "Data Source=THEKILLERCODE;Initial Catalog=Experimentacion;Integrated Security=True;\r\n"; // Reemplaza esto con tu cadena de conexión a la base de datos
+
         public Login()
         {
             InitializeComponent();
+        }
+
+        private void LoginBtn_Click(object sender, EventArgs e)
+        {
+            string usuario = UserTextBox.Text;
+            string clave = PasswordTextBox.Text;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT ID_Usuario FROM Usuario WHERE Usuario = @Usuario AND Clave = @Clave";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Usuario", usuario);
+                command.Parameters.AddWithValue("@Clave", clave);
+
+                object result = command.ExecuteScalar();
+
+                if (result != null)
+                {
+                    // Las credenciales son válidas
+                    MessageBox.Show("Inicio de sesión exitoso");
+                    // Puedes abrir el formulario principal aquí o realizar cualquier otra acción necesaria.
+                }
+                else
+                {
+                    // Las credenciales no son válidas
+                    MessageBox.Show("Nombre de usuario o contraseña incorrectos");
+                }
+            }
         }
     }
 }
